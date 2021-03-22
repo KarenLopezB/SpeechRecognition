074489ea5d0f310ddfe1386cfe5593f5cd931cd9
Karen Daniela López <Karen_lopez1936@hotmail.com>
2021-03-21 18:44:28 -0700

    Sale en español pero no escribe bien lo que dice

--- recognizer.py
+++ recognizer.py
@@ -6,14 +6,14 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 #definimos archivo de audio
-audio_file = sr.AudioFile('boo-gatito.wav')
+audio_file = sr.AudioFile('shrek.wav')
 
 #speech recognition
 with audio_file as source:
     r.adjust_for_ambient_noise(source)
     audio = r.record(source)
-    result = r.recognize_google(audio)
-    #result = r.recognize_google(audio, language="en-US")
+    #result = r.recognize_google(audio)
+    result = r.recognize_google(audio, language="es-MX")
 
 #exportar los resultados
 with open('result.txt', mode='w') as file:
